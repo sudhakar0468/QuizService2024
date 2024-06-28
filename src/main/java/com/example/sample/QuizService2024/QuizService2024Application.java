b70a487890a0a96a73e1996660c5d71613d0988e
sudhakar0468 <pogula.sudhakarreddy@gmail.com>
2024-06-28 11:50:20 +0530

    initial commit in quiz services

---
+++ src/main/java/com/example/sample/QuizService2024/QuizService2024Application.java
@@ -0,0 +1,13 @@
+package com.example.sample.QuizService2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizService2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizService2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample/QuizService2024/QuizService2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample.QuizService2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizService2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
